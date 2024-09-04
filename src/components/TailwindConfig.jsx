48480722e7e238b42c0fca3dd2fc21c43b7731b0
paku <p4blo.kuhn@gmail.com>
2024-09-04 17:38:45 -0300

    updated tailwind config description, and turned into ul and li the labels

--- src/components/TailwindConfig.jsx
+++ src/components/TailwindConfig.jsx
@@ -14,23 +14,27 @@ export default function TailwindConfig({ children, name = 'test' }) {
 
       <dialog className='bg-black w-96 p-5 rounded-lg m-auto' ref={refModal}>
         <h1 className='text-slate-400 text-xl font-bold'>Tailwind Config</h1>
+        <p className='text-slate-200'>
+          Agrega en tu archivo tailwind.config.js dentro de theme y extend{' '}
+        </p>
         <div className='text-slate-200 bg-slate-800 rounded-lg my-2 pl-5 flex flex-col'>
-          <p>{`colors: {`}</p>
-          <p className='pl-3'>{`${name}: {`}</p>
-          <div className='pl-5'>
-            <p>{`100: 'var(${name}-100)',`}</p>
-            <p>{`200: 'var(${name}-200)',`}</p>
-            <p>{`300: 'var(${name}-300)',`}</p>
-            <p>{`400: 'var(${name}-400)',`}</p>
-            <p>{`500: 'var(${name}-500)',`}</p>
-            <p>{`600: 'var(${name}-600)',`}</p>
-            <p>{`700: 'var(${name}-700)',`}</p>
-            <p>{`800: 'var(${name}-800)',`}</p>
-            <p>{`900: 'var(${name}-900)',`}</p>
-            <p>{`},`}</p>
-          </div>
+          <span>{`colors: {`}</span>
+          <span className='pl-3'>{`${name}: {`}</span>
+          <ul className='pl-5'>
+            <li>{`100: 'var(${name}-100)',`}</li>
+            <li>{`200: 'var(${name}-200)',`}</li>
+            <li>{`300: 'var(${name}-300)',`}</li>
+            <li>{`400: 'var(${name}-400)',`}</li>
+            <li>{`500: 'var(${name}-500)',`}</li>
+            <li>{`600: 'var(${name}-600)',`}</li>
+            <li>{`700: 'var(${name}-700)',`}</li>
+            <li>{`800: 'var(${name}-800)',`}</li>
+            <li>{`900: 'var(${name}-900)',`}</li>
+            <li>{`},`}</li>
+          </ul>
           <p>{`}`}</p>
         </div>
+
         <button
           className='w-full bg-red-500 rounded-lg mt-2 text-white py-2'
           onClick={closeModal}
