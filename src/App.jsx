ce6aeb7f6afceb2c34cc43dc8b7052dd96c47e48
paku <p4blo.kuhn@gmail.com>
2024-09-04 17:18:10 -0300

    created title component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { handleCopy } from './libs/handleCopy'
 import { hexToHSL } from './libs/hexToHSL'
 import { hslToHex } from './libs/hslToHex'
 import TailwindConfig from './components/TailwindConfig'
+import Title from './components/Title'
 
 const MIN_RANGE = 1
 const MAX_RANGE = 10
@@ -34,21 +35,7 @@ function ColorGradient() {
   return (
     <>
       <header className='text-white w-full h-full flex items-center flex-col py-5 gap-2'>
-        <h1 className=' flex items-center font-thin justify-center text-8xl text-center py-5  text-blue-500 stroke-blue-500 fill-blue-500 '>
-          <svg
-            className='w-32 h-32'
-            viewBox='0 0 24 24'
-            strokeWidth='0.4'
-            fill='transparent'
-            strokeLinecap='round'
-            strokeLinejoin='round'
-          >
-            <path stroke='none' d='M0 0h24v24H0z' fill='none' />
-            <path d='M11.667 6c-2.49 0 -4.044 1.222 -4.667 3.667c.933 -1.223 2.023 -1.68 3.267 -1.375c.71 .174 1.217 .68 1.778 1.24c.916 .912 2 1.968 4.288 1.968c2.49 0 4.044 -1.222 4.667 -3.667c-.933 1.223 -2.023 1.68 -3.267 1.375c-.71 -.174 -1.217 -.68 -1.778 -1.24c-.916 -.912 -1.975 -1.968 -4.288 -1.968zm-4 6.5c-2.49 0 -4.044 1.222 -4.667 3.667c.933 -1.223 2.023 -1.68 3.267 -1.375c.71 .174 1.217 .68 1.778 1.24c.916 .912 1.975 1.968 4.288 1.968c2.49 0 4.044 -1.222 4.667 -3.667c-.933 1.223 -2.023 1.68 -3.267 1.375c-.71 -.174 -1.217 -.68 -1.778 -1.24c-.916 -.912 -1.975 -1.968 -4.288 -1.968z' />
-          </svg>
-          <img className='w-0' src='/public/favicon.webp' alt='' />
-          Palete
-        </h1>
+        <Title>Palete</Title>
         <section className='w-80 space-y-3'>
           <div className='w-80 color-picker-ctn '>
             <input
