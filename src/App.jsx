3db8c22753ccf67f1567947c0f5613edbfae9a68
Pαblo Kuhn <p4blo.kuhn@gmail.com>
2024-10-17 23:32:43 -0300

    removed title, removed 1000, new label style

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,9 @@ import { handleCopy } from './libs/handleCopy'
 import { hexToHSL } from './libs/hexToHSL'
 import { hslToHex } from './libs/hslToHex'
 import TailwindConfig from './components/TailwindConfig'
-import Title from './components/Title'
 
 const MIN_RANGE = 1
-const MAX_RANGE = 10
+const MAX_RANGE = 9
 function ColorGradient() {
   const [baseColor, setBaseColor] = useState('#42a4ff')
   const [colorName, setColorName] = useState('primary')
@@ -34,14 +33,27 @@ function ColorGradient() {
 
   return (
     <>
-      <header className=' relative bg-gray-800  text-white w-full h-full flex items-center flex-col py-5 gap-2'>
-        <Title>Palette</Title>
-        <section className='w-80 space-y-4 pb-6'>
+      <header className=' py-20 relative bg-gray-800  text-white w-full h-full flex items-center flex-col'>
+        <h1 className=' flex items-center justify-center text-xl lg:text-8xl text-center  text-white pb-10 stroke-white fill-white font-thin'>
+          <svg
+            className='size-24 lg:size-32'
+            viewBox='0 0 24 24'
+            strokeWidth='0.4'
+            fill='transparent'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+          >
+            <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+            <path d='M11.667 6c-2.49 0 -4.044 1.222 -4.667 3.667c.933 -1.223 2.023 -1.68 3.267 -1.375c.71 .174 1.217 .68 1.778 1.24c.916 .912 2 1.968 4.288 1.968c2.49 0 4.044 -1.222 4.667 -3.667c-.933 1.223 -2.023 1.68 -3.267 1.375c-.71 -.174 -1.217 -.68 -1.778 -1.24c-.916 -.912 -1.975 -1.968 -4.288 -1.968zm-4 6.5c-2.49 0 -4.044 1.222 -4.667 3.667c.933 -1.223 2.023 -1.68 3.267 -1.375c.71 .174 1.217 .68 1.778 1.24c.916 .912 1.975 1.968 4.288 1.968c2.49 0 4.044 -1.222 4.667 -3.667c-.933 1.223 -2.023 1.68 -3.267 1.375c-.71 -.174 -1.217 -.68 -1.778 -1.24c-.916 -.912 -1.975 -1.968 -4.288 -1.968z' />
+          </svg>
+          Pallete
+        </h1>
+        <section className='w-80 space-y-4 '>
           <div className='w-80 color-picker-ctn '>
             <input
               type='text'
               style={{ background: baseColor }}
-              className={`border-2 w-full border-gray-700 px-5 py-3 rounded-lg  outline-none`}
+              className={`border-2 w-full border-gray-700 px-5 py-2 rounded-lg  outline-none`}
               value={baseColor}
               onChange={(e) => setBaseColor(e.target.value)}
             />
@@ -53,7 +65,7 @@ function ColorGradient() {
             />
           </div>
           <input
-            className='border-2 border-gray-700 w-full py-3 bg-gray-900 text-white  px-4 rounded-lg'
+            className='border-2 border-gray-700 w-full py-2 bg-gray-900 text-white  px-4 rounded-lg'
             type='text'
             value={colorName}
             onChange={(e) => setColorName(e.target.value)}
@@ -62,12 +74,12 @@ function ColorGradient() {
             <input
               className='range'
               type='range'
-              min={4}
-              max={13}
+              min={6}
+              max={11}
               value={range}
               onChange={(e) => setRange(e.target.value)}
             />
-            <p className='font-bold w-28 text-white '>Brightness</p>
+            <p className='font-bold w-28 text-gray-600 '>Brightness</p>
           </div>
 
           <div className='flex gap-2'>
@@ -82,14 +94,14 @@ function ColorGradient() {
         <main className='w-full   black color-wrapper'>
           {Object.entries(gradientColors).map(([key, value]) => (
             <div
-              className={` border-gray-800 flex h-full py-3  justify-start items-center`}
+              className={` border-gray-800 flex h-full py-5  justify-start items-center`}
               key={key}
               style={{ backgroundColor: value }}
             >
               <p
                 className={`${
-                  key.includes('500') && 'font-bold'
-                } color-row flex justify-start items-center bg-gray-900 w-60 rounded-r-xl `}
+                  key.includes('500') && 'border-l-4  pl-2 '
+                } h-10 text-white  flex justify-start pl-2 items-center bg-gray-900/60 backdrop-blur-md w-60 rounded-r-xl `}
               >
                 {key}: {value};
               </p>
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,18 +2,8 @@ export default function Title({ children }) {
   return (
     <>
       <h1 className=' flex items-center justify-center text-6xl lg:text-8xl text-center py-5 text-white stroke-white fill-white font-thin'>
-        <svg
-          className='size-24 lg:size-32'
-          viewBox='0 0 24 24'
-          strokeWidth='0.4'
-          fill='transparent'
-          strokeLinecap='round'
-          strokeLinejoin='round'
-        >
-          <path stroke='none' d='M0 0h24v24H0z' fill='none' />
-          <path d='M11.667 6c-2.49 0 -4.044 1.222 -4.667 3.667c.933 -1.223 2.023 -1.68 3.267 -1.375c.71 .174 1.217 .68 1.778 1.24c.916 .912 2 1.968 4.288 1.968c2.49 0 4.044 -1.222 4.667 -3.667c-.933 1.223 -2.023 1.68 -3.267 1.375c-.71 -.174 -1.217 -.68 -1.778 -1.24c-.916 -.912 -1.975 -1.968 -4.288 -1.968zm-4 6.5c-2.49 0 -4.044 1.222 -4.667 3.667c.933 -1.223 2.023 -1.68 3.267 -1.375c.71 .174 1.217 .68 1.778 1.24c.916 .912 1.975 1.968 4.288 1.968c2.49 0 4.044 -1.222 4.667 -3.667c-.933 1.223 -2.023 1.68 -3.267 1.375c-.71 -.174 -1.217 -.68 -1.778 -1.24c-.916 -.912 -1.975 -1.968 -4.288 -1.968z' />
-        </svg>
-        <img className='w-0' src='/public/favicon.webp' alt='' />
+ 
+
         {children}
       </h1>
     </>
