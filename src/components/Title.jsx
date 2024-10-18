6839e5eda177454106d6d969f5c08a5507669f9b
Pαblo Kuhn <p4blo.kuhn@gmail.com>
2024-10-17 23:43:23 -0300

    updated and added new font

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,8 +1,7 @@
 export default function Title({ children }) {
   return (
     <>
-      <h1 className=' flex items-center justify-center text-6xl lg:text-8xl text-center py-5 text-white stroke-white fill-white font-thin'>
- 
+      <h1 className=' flex items-center justify-center text-6xl lg:text-8xl text-center py-5 text-white stroke-white fill-white '>
 
         {children}
       </h1>
